fe087277825f7b353182a045d4b3c0736614ba70
hanmsywll <116424776+hanmsywll@users.noreply.github.com>
2024-06-19 07:19:07 +0700

    Delete utils directory

--- utils/Response.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Response {
-    public static function send($data, $status_code = 200) {
-        http_response_code($status_code);
-        echo json_encode($data);
-        exit;
-    }
-}
